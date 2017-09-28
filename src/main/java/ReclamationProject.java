f2ba06cee72d5e6eb629b0b0b291f8ee1a5b8f8e
rkulkarni1 <reva_kulkarni@yahoo.com>
2017-09-28 18:48:33 -0500

    Updated code

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,37 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+     * This class needs to be fixed.
+*/
+public class ReclamationProject {
+
+    /**
+     *@param s1 some String value.
+     *@param s2 some String value.
+     *@return r return string.
+     */
+    public static String longestCommonSubstring(final String s1, final String s2) {
+        String a = s1;
+        String b = s2;
+
+        if (s1.length() > s2.length()) {
+            String c = s1;
+            a = b;
+            b = c;
+        }
+
+        String r = "";
+
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if ((a.regionMatches(i, b, k, j) && j > r.length())) {
+                        r = (a.substring(i, i + j));
+                    }
+                }
+            }
+        }
+        return r;
+    }
 }
